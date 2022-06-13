764fb21badc8476a4c3aa50cefa3e8b6a85815fb
Amit Singh <amisingh@guidewire.com>
2022-06-13 12:30:01 +0530

    adds LonelyInteger

---
+++ src/google/LonelyInteger.java
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-flipping-bits/problem?h_l=interview&h_r=next-challenge&h_v=zen&isFullScreen=true&playlist_slugs%5B%5D%5B%5D=preparation-kits&playlist_slugs%5B%5D%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D%5B%5D=one-month-week-one
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+class LonelyInteger {
+    public static void main(String[] args) {
+        Integer[] arr = {1,2,3,4,3,2,1};
+        List<Integer> a = Arrays.asList(arr);
+        int[] freqs = new int[101];
+
+        a.forEach(ele -> freqs[ele] += 1);
+        
+        for(int i = 0; i < freqs.length; i++) {
+            if(freqs[i] == 1) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
