12ec474b073678b034bad92d176c07a298cf238d
semmet95 <singhamitch@gmail.com>
2022-06-18 23:49:23 +0530

    adds google collection

--- src/google/Test.java
+++ src/google/Test.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Test {
     public static void main(String[] args) {
@@ -20,5 +22,16 @@ public class Test {
         }
 
         System.out.println(allChars.size());
+    }
+
+    public static int flippingMatrix(List<Integer> arr) {
+        int sum = 0;
+        
+        List<Integer> diffs =  IntStream.range(0, arr.size()-1)
+            .map(i -> arr.get(i+1) - arr.get(i))
+            .boxed()
+            .collect(Collectors.toList());
+    
+        return sum;
     }
 }
